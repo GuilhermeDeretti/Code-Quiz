8370bc04f3659065aa355e376f3caa3d1c84be9b
Guitti <guilhermederetti9@gmail.com>
2022-12-03 14:56:49 +0000

    added fixed choices, TODO: make dinamic

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,31 +1,44 @@
 var timeEl = document.querySelector("#time");
-var startScreen = document.querySelector("#start-screen");
-var startButton = document.querySelector("#start");
-var questions = document.querySelector("#questions");
-var endScreen = document.querySelector("#end-screen");
+var startScreenEl = document.querySelector("#start-screen");
+var startButtonEl = document.querySelector("#start");
+var questionsEl = document.querySelector("#questions");
+var questionTitleEl = document.querySelector("#question-title");
+var choicesEl = document.querySelector("#choices");
+var endScreenEl = document.querySelector("#end-screen");
 
-startButton.addEventListener("click", startGame);
+startButtonEl.addEventListener("click", startGame);
 
-function startGame(){
+function startGame() {
     // set timer
     setTime();
     // hide startScreen
-    startScreen.setAttribute("class", "hide");
-    // show first question
-    
+    startScreenEl.setAttribute("class", "hide");
+    // show question area
+    questionsEl.setAttribute("class", "start");
+    //create questions
+    questionTitleEl.textContent = "question test"
+
+    var choice1 = document.createElement("button");
+    var choice2 = document.createElement("button");
+
+    choice1.textContent = "choice 1";
+    choice2.textContent = "choice 2";
+
+    choicesEl.appendChild(choice1);
+    choicesEl.appendChild(choice2);
 }
 
-function setTime(){
+function setTime() {
     secondsLeft = 70;
-    var timerInterval = setInterval(function() {
+    var timerInterval = setInterval(function () {
         secondsLeft--;
         timeEl.textContent = secondsLeft;
-    
-        if(secondsLeft === 0) {
-          // Stops execution of action at set interval
-          clearInterval(timerInterval);
-          // Calls function to Show score;
+
+        if (secondsLeft === 0) {
+            // Stops execution of action at set interval
+            clearInterval(timerInterval);
+            // Calls function to Show score;
         }
-    
-    }, 1000);    
+
+    }, 1000);
 }
